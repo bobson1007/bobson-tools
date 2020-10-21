9ffa13462968d79990a6f7ba9e7788e8f87aecd9
Gat <ssssseeeee1007@gmail.com>
2020-10-21 23:05:57 +0800

    第三牌微調

--- src/main/java/priv/bobson/tools/ptcgcard/DeckSupplier.java
+++ src/main/java/priv/bobson/tools/ptcgcard/DeckSupplier.java
@@ -78,83 +78,86 @@ public class DeckSupplier implements Supplier<Map> {
      * 顫弦蠑螈vmax + 銀伴戰獸
      */
     private static void init2() {
-        //pm - 16
+        //pm - 17
         deck.put("顫弦蠑螈v", 3);
         deck.put("顫弦蠑螈vmax", 3);
         deck.put("呆殼獸v", 2);
         deck.put("屬性空", 2);
         deck.put("銀伴戰獸GX", 2);
-        deck.put("逐電犬v", 3);
+        deck.put("逐電犬v", 1);
         deck.put("捷拉奧拉GX", 1);
+        deck.put("閃電鳥", 2);
+        deck.put("壺壺", 1);
 
         //場地 - 1
         deck.put("雷霆山", 1);
 //        deck.put("礦山", 2);
 
-        //物品 - 21
+        //物品 - 19
         deck.put("先機球", 4);
-        deck.put("通信", 1);
+        deck.put("巢穴球", 2);
+        deck.put("通信", 2);
         deck.put("薰香", 2);
         deck.put("交替", 4);
-        deck.put("粉碎之錘", 4);
-        deck.put("寶可夢捕捉器", 4);
-        deck.put("印章", 1);
-        deck.put("朋友手冊", 1);
+//        deck.put("粉碎之錘", 4);
+//        deck.put("寶可夢捕捉器", 4);
+//        deck.put("印章", 1);
+//        deck.put("朋友手冊", 1);
 //        deck.put("電氣充電器", 3);
-//        deck.put("電氣力量", 4);
+        deck.put("電氣力量", 4);
 //        deck.put("吹風機", 1);
 
         //裝備 - 0
 //        deck.put("耳鳴鈴鐺", 2);
 
         //人物 - 12
-        deck.put("博士", 2);
-        deck.put("瑪俐", 2);
+        deck.put("博士", 4);
         deck.put("瑪奧水蓮", 2);
         deck.put("養鳥人", 2);
         deck.put("洛茲", 2);
         deck.put("老大", 2);
 
-        //能量 - 10
+        //能量 - 12
         deck.put("高速電能", 4);
-        deck.put("電能", 6);
+        deck.put("電能", 8);
     }
 
     /**
-     * 雷公ar + 逐電犬v + 多邊獸z
+     * 雷公ar + 多邊獸z
      */
     private static void init3() {
         //PM - 16
         deck.put("雷公ar", 4);
-        deck.put("基拉祈", 3);
+        deck.put("基拉祈", 2);
         deck.put("多邊獸", 2);
         deck.put("多邊獸z", 2);
         deck.put("屬性空", 2);
         deck.put("銀伴戰獸GX", 2);
         deck.put("四顎tt", 1);
+        deck.put("鳴鳴v", 1);
 
         //場地 - 1
         deck.put("雷霆山", 1);
 
-        //物品 - 15
+        //物品 - 16
         deck.put("先機球", 4);
-        deck.put("巢穴球", 1);
+        deck.put("巢穴球", 2);
         deck.put("通信", 2);
         deck.put("薰香", 2);
         deck.put("交替", 3);
         deck.put("神奇糖果", 3);
 
-        //道具 - 2
-        deck.put("逃脫滑板", 2);
+        //道具 - 1
+        deck.put("逃脫滑板", 1);
 
-        //人物 - 11
-        deck.put("竹蘭", 4);
+        //人物 - 10
+        deck.put("竹蘭", 3);
         deck.put("瑪俐", 3);
         deck.put("養鳥人", 2);
         deck.put("老大", 2);
 
-        //能量 - 15
-        deck.put("彩虹能量", 3);
+        //能量 - 16
+        deck.put("彩虹能量", 4);
         deck.put("高速電能", 4);
         deck.put("單位能量", 4);
         deck.put("極光能量", 4);
